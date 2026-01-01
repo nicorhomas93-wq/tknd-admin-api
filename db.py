b1a28ec2f25a5c349b06f690ba52d993a8c8fab6
nicorhomas93-wq <nicorhomas93@gmail.com>
2026-01-02 00:04:21 +0100

    Initialize database connection with SQLAlchemy

---
+++ db.py
@@ -0,0 +1,8 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.environ["DATABASE_URL"]  # kommt aus Render ENV
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
